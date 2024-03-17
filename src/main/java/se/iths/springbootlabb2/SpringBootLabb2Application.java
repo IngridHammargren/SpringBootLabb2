75c11a401a0df9c17d8dff8e79c124d820fe5dbb
IngridHammargren <ingrid.hammargren@iths.se>
2024-03-17 21:17:45 +0100

    Initial commit

---
+++ src/main/java/se/iths/springbootlabb2/SpringBootLabb2Application.java
@@ -0,0 +1,13 @@
+package se.iths.springbootlabb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLabb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLabb2Application.class, args);
+    }
+
+}
