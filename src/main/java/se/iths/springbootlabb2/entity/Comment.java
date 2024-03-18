7d7ba1653a01f012f4e7d97730f294215c423e58
Hatice Celik <hatice.celik@iths.se>
2024-03-18 11:33:47 +0100

    add equals, hashcode and toString in all entity
    Co-authored-by: Ingrid Hammargren <ingrid.hammargren@iths.se>
    Co-authored-by: Max Erkmar <max.erkmar@iths.se>
    Co-authored-by: Alfred Bild <alfredbild@gmail.com>

--- src/main/java/se/iths/springbootlabb2/entity/Comment.java
+++ src/main/java/se/iths/springbootlabb2/entity/Comment.java
@@ -3,8 +3,10 @@ package se.iths.springbootlabb2.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -33,4 +35,28 @@ public class Comment {
     @Column(name = "last_edit_date")
     private Instant lastEditDate;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Comment comment = (Comment) o;
+        return getId() != null && Objects.equals(getId(), comment.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "content = " + content + ", " +
+                "creationDate = " + creationDate + ", " +
+                "lastEditDate = " + lastEditDate + ")";
+    }
 }
--- src/main/java/se/iths/springbootlabb2/entity/Friend.java
+++ src/main/java/se/iths/springbootlabb2/entity/Friend.java
@@ -3,8 +3,10 @@ package se.iths.springbootlabb2.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -33,4 +35,28 @@ public class Friend {
     @Column(name = "response_date")
     private Instant responseDate;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Friend friend = (Friend) o;
+        return getId() != null && Objects.equals(getId(), friend.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "status = " + status + ", " +
+                "requestDate = " + requestDate + ", " +
+                "responseDate = " + responseDate + ")";
+    }
 }
