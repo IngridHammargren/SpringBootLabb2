ae3db9fbf5662d0a84dcc545c2b9b0dccc9b72ff
Hatice Celik <hatice.celik@iths.se>
2024-03-21 11:30:14 +0100

    edit entity får Message and User

--- src/main/java/se/iths/springbootlabb2/user/User.java
+++ src/main/java/se/iths/springbootlabb2/user/User.java
@@ -1,70 +1,76 @@
 package se.iths.springbootlabb2.user;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.proxy.HibernateProxy;
+import jakarta.persistence.*;
+import se.iths.springbootlabb2.message.Message;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.time.Instant;
-import java.util.Objects;
-
-@Getter
-@Setter
 @Entity
 public class User {
     @Id
-    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username", nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
-    @Column(name = "email", nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password", nullable = false)
+    @Column(nullable = false)
     private String password;
 
-    @Column(name = "display_name")
     private String displayName;
-
-    @Column(name = "profile_picture")
     private String profilePicture;
 
-    @Column(name = "registration_date")
-    private Instant registrationDate;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Message> messages = new ArrayList<>();
 
-    @Column(name = "last_login_date")
-    private Instant lastLoginDate;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        User user = (User) o;
-        return getId() != null && Objects.equals(getId(), user.getId());
+    public Long getId() {
+        return id;
     }
 
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "(" +
-                "id = " + id + ", " +
-                "username = " + username + ", " +
-                "email = " + email + ", " +
-                "password = " + password + ", " +
-                "displayName = " + displayName + ", " +
-                "profilePicture = " + profilePicture + ", " +
-                "registrationDate = " + registrationDate + ", " +
-                "lastLoginDate = " + lastLoginDate + ")";
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
     }
 }
