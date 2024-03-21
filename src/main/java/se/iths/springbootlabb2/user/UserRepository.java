e9385ca4336f5a32990e9aa1e63b3691d0c2bd86
Hatice Celik <hatice.celik@iths.se>
2024-03-21 10:07:00 +0100

    add Repository

---
+++ src/main/java/se/iths/springbootlabb2/user/UserRepository.java
@@ -0,0 +1,10 @@
+package se.iths.springbootlabb2.user;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends ListCrudRepository<User, Long>{
+    List<User> findByName(String name);
+
+}
