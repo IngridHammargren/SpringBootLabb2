ce4dee856deeb9da7ad21a18fa9b6dc531ffcc5a
maxer <max.erkmar@iths.se>
2024-03-25 10:57:31 +0100

    updated MessageRepository

--- src/main/java/se/iths/springbootlabb2/repositories/MessageRepository.java
+++ src/main/java/se/iths/springbootlabb2/repositories/MessageRepository.java
@@ -1,7 +1,14 @@
 package se.iths.springbootlabb2.repositories;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
 import se.iths.springbootlabb2.entities.MessageEntity;
 
-public interface MessageRepository extends ListCrudRepository<MessageEntity, Long> {
+
+import java.util.List;
+
+public interface MessageRepository extends CrudRepository<MessageEntity, Long> {
+
 }
