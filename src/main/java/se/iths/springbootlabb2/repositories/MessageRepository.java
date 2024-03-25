5ab826ada2ce477e42c3ba2309769e007f5b3ffb
Hatice Celik <hatice.celik@iths.se>
2024-03-25 11:48:17 +0100

    update MessageRepository

--- src/main/java/se/iths/springbootlabb2/repositories/MessageRepository.java
+++ src/main/java/se/iths/springbootlabb2/repositories/MessageRepository.java
@@ -1,14 +1,24 @@
 package se.iths.springbootlabb2.repositories;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import se.iths.springbootlabb2.entities.MessageEntity;
-
-
+import java.awt.print.Pageable;
 import java.util.List;
 
-public interface MessageRepository extends CrudRepository<MessageEntity, Long> {
+public interface MessageRepository extends ListCrudRepository<MessageEntity, Long> {
+
+
+    @Query ("SELECT m FROM MessageEntity m WHERE m.userEntity.id = :userId")
+    List<MessageEntity> findByUserId(Long userId);
+
+    @Query("SELECT m FROM MessageEntity m WHERE m.userEntity.id = :userId AND m.isPublic = true")
+    List<MessageEntity> findPublicMessagesByUserId(Long userId);
+
+    @Query("SELECT m FROM MessageEntity m WHERE m.isPublic = true ORDER BY m.createdDate DESC")
+    List<MessageEntity> findLatestMessagesPublic(java.awt.print.Pageable pageable);
+
+    @Query("SELECT m FROM MessageEntity m WHERE m.isPublic = false ORDER BY m.createdDate DESC")
+    List<MessageEntity> findLatestMessagesNotPublic(Pageable pageable);
 
 }
