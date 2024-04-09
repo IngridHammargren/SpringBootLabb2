6f1e883cd74a7d58a7922a3177fa3b656f558309
Ingrid <ingrid.hammargren@iths.se>
2024-04-09 13:47:18 +0200

    Clean up user

--- src/main/java/se/iths/springbootlabb2/repositories/UserRepository.java
+++ src/main/java/se/iths/springbootlabb2/repositories/UserRepository.java
@@ -12,34 +12,6 @@ import se.iths.springbootlabb2.entities.UserEntity;
 import java.util.List;
 
 public interface UserRepository  extends ListCrudRepository<UserEntity, Long> {
-    List<UserEntity> findByUserName(String userName);
 
-    List<UserEntity> findByEmail(String email);
-
-    List<UserEntity> findByFirstName (String firstName);
-
-    List<UserEntity> findByLastName(String lastName);
-
-
-
-
-    @Modifying
-    @Transactional
-    @Query(value = "INSERT INTO user_entity (user_name, profile_picture, first_name, last_name, email) VALUES (:userName, :profilePicture, :firstName, :lastName, :email)", nativeQuery = true)
-    void newUser(@Param("userName") String userName, @Param("profilePicture") String profilePicture, @Param("firstName") String firstName, @Param("lastName") String lastName, @Param("email") String email);
-
-    @Modifying
-    @Transactional
-    @Query("UPDATE UserEntity u SET u.profilePicture = :profilePicture WHERE u.id = :id")
-    void updateProfilePictureByUserName(@Param("id") Long id, @Param("profilePicture") String profilePicture);
-
-    @Modifying
-    @Transactional
-    @Query("UPDATE UserEntity u SET u.userName = :userName WHERE u.id = :id")
-    void updateUserNameByUserName(@Param("id") Long id, @Param("userName") String userName);
-
-    @Modifying
-    @Transactional
-    @Query("UPDATE UserEntity u SET u.email = :email WHERE u.id = :id")
-    void updateEmailById(@Param("id") Long id, @Param("email") String email);
 }
+
